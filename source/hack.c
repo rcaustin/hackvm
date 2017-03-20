0e7e24d0ff2a4dca99ebec76622aed36aef261eb
Ryan Austin <ryan.rcaustin@gmail.com>
2017-03-20 00:08:04 -0400

    Fix Condition on JUMP Ops; Add Else-Case to JUMP Ops

--- source/hack.c
+++ source/hack.c
@@ -63,81 +63,62 @@ void execute_instruction(WORD instruction) {
 
   if ((instruction & INST_C) == INST_C) {
     /* C-INSTRUCTION */
-    // printf("C-INSTRUCTION:  ");
     WORD result = 0;
 
     /* Computation */
     switch(instruction & COMP_MASK) {
     case COMP_ZERO:
-      // printf("COMP_ZERO ");
       result += 0;
       break;
     case COMP_POS1:
-      // printf("COMP_POS1 ");
       result += 1;
       break;
     case COMP_NEG1:
-      // printf("COMP_NEG1 ");
       result -= 1;
       break;
     case COMP_D:
-      // printf("COMP_D    ");
       result = D_REGISTER;
       break;
     case COMP_A:
-      // printf("COMP_A    ");
       result = A_REGISTER;
       break;
     case COMP_CMPD:
-      // printf("COMP_CMPD ");
       result = ~D_REGISTER;
       break;
     case COMP_CMPA:
-      // printf("COMP_CMPA ");
       result = ~A_REGISTER;
       break;
     case COMP_NEGD:
-      // printf("COMP_NEGD ");
       result = D_REGISTER ^ SIGN_MASK;
       break;
     case COMP_NEGA:
-      // printf("COMP_NEGA ");
       result = A_REGISTER ^ SIGN_MASK;
       break;
     case COMP_INCD:
-      // printf("COMP_INCD ");
       result = D_REGISTER + 1;
       break;
     case COMP_INCA:
-      // printf("COMP_INCA ");
       result = A_REGISTER + 1;
       break;
     case COMP_DECD:
-      // printf("COMP_DECD ");
       result = D_REGISTER - 1;
       break;
     case COMP_DECA:
-      // printf("COMP_DECA ");
       result = A_REGISTER - 1;
       break;
     case COMP_DPA:
-      // printf("COMP_DPA  ");
       result = D_REGISTER + A_REGISTER;
       break;
     case COMP_DMA:
-      // printf("COMP_DMA  ");
       result = D_REGISTER - A_REGISTER;
       break;
     case COMP_AMD:
-      // printf("COMP_AMD  ");
       result = A_REGISTER - D_REGISTER;
       break;
     case COMP_DAA:
-      // printf("COMP_DAA  ");
       result = D_REGISTER & A_REGISTER;
       break;
     case COMP_DOA:
-      // printf("COMP_DOA  ");
       result = D_REGISTER | A_REGISTER;
       break;
     case COMP_M:
@@ -169,101 +150,100 @@ void execute_instruction(WORD instruction) {
       break;
     case COMP_DOM:
       result = D_REGISTER | RAM[A_REGISTER];
+      break;
     default:
-      log_error("INVALID COMPUTATION INSTRUCTION");
+      printf("INVALID COMPUTATION INSTRUCTION:  ");
+      print_word(instruction, 4);
       break;
     }
 
     /* Destination */
     switch(instruction & DEST_MASK) {
     case DEST_NUL:
-      // printf("DEST_NUL ");
       break;
     case DEST_M:
-      // printf("DEST_M   ");
       RAM[A_REGISTER] = result;
       break;
     case DEST_D:
-      // printf("DEST_D   ");
       D_REGISTER = result;
       break;
     case DEST_MD:
-      // printf("DEST_MD  ");
       RAM[A_REGISTER] = result;
       D_REGISTER = result;
       break;
     case DEST_A:
-      // printf("DEST_A   ");
       A_REGISTER = result;
       break;
     case DEST_AM:
-      // printf("DEST_AM  ");
       RAM[A_REGISTER] = result;
       A_REGISTER = result;
       break;
     case DEST_AD:
-      // printf("DEST_AD  ");
       A_REGISTER = result;
       D_REGISTER = result;
       break;
     case DEST_AMD:
-      // printf("DEST_AMD ");
       D_REGISTER = result;
       RAM[A_REGISTER] = result;
       A_REGISTER = result;
       break;
     default:
       log_error("INVALID DESTINATION INSTRUCTION");
+      print_word(instruction, 4);
       break;
     }
 
     /* Jump */
     switch(instruction & JUMP_MASK) {
     case JUMP_NUL:
-      // printf("JUMP_NUL\n");
       ++PC_REGISTER;
       break;
     case JUMP_JGT:
-      // printf("JUMP_JGT\n");
-      if (result > SIGN_MASK)
+      if ((short)result > 0)
         PC_REGISTER = A_REGISTER;
+      else
+        ++PC_REGISTER;
       break;
     case JUMP_JEQ:
-      // printf("JUMP_JEQ\n");
-      if (result == SIGN_MASK)
+      if ((short)result == 0)
         PC_REGISTER = A_REGISTER;
+      else
+        ++PC_REGISTER;
       break;
     case JUMP_JGE:
-      // printf("JUMP_JGE\n");
-      if (result >= SIGN_MASK)
+      if ((short)result >= 0)
         PC_REGISTER = A_REGISTER;
+      else
+        ++PC_REGISTER;
       break;
     case JUMP_JLT:
-      // printf("JUMP_JLT\n");
-      if (result < SIGN_MASK)
+      if ((short)result < 0)
         PC_REGISTER = A_REGISTER;
+      else
+        ++PC_REGISTER;
       break;
     case JUMP_JNE:
-      // printf("JUMP_JNE\n");
-      if (result != SIGN_MASK)
+      if ((short)result != 0)
         PC_REGISTER = A_REGISTER;
+      else
+        ++PC_REGISTER;
       break;
     case JUMP_JLE:
-      // printf("JUMP_JLE\n");
-      if (result <= SIGN_MASK)
+      if ((short)result <= 0)
         PC_REGISTER = A_REGISTER;
+      else
+        ++PC_REGISTER;
       break;
     case JUMP_JMP:
-      // printf("JUMP_JMP\n");
       PC_REGISTER = A_REGISTER;
       break;
     default:
       log_error("INVALID JUMP INSTRUCTION");
+      print_word(instruction, 4);
       break;
     }
   } else {
     /* A-INSTRUCTION */
-    // printf("A-INSTRUCTION:  SET A_REGISTER TO %hu\n", instruction);
     A_REGISTER = instruction;
     ++PC_REGISTER;
   }
