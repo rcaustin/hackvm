14919831cc9eb636bc0023598992d3a341b2bfcb
Ryan Austin <ryan.rcaustin@gmail.com>
2017-04-10 10:28:33 -0400

    Add A-Instruction Parsing and Write to ASM Output

--- source/assembler.c
+++ source/assembler.c
@@ -1,6 +1,7 @@
 #include "assembler.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "logging.h"
 #include "fileio.h"
@@ -8,15 +9,50 @@
 
 int main(int argc, char* argv[]) {
 
-  if (argc != 2)
+  if (argc < 2 || argc > 3)
     fatal_error("INVALID NUMBER OF ARGUMENTS");
 
-  signed long line_count  = 0;
-  char** hasm_source = read_file_by_line(argv[1], &line_count);
+  long line_count  = 0;
+  char** source = read_text_file(argv[1], &line_count);
+  WORD* instructions = calloc(line_count, sizeof(WORD));
 
-  for (long i = 0; i < line_count; i++)
-    if (hasm_source[i])
-      printf("LINE %li:  %s\n", line_count, hasm_source[i]);
+  for (signed long i = 0; i < line_count; i++)
+    instructions[i] = parse_hack_asm(source[i]);
+
+  /* TODO: WRITE INSTRUCTIONS TO A.OUT */
+  int success = 0;
+  char* output = (argc == 3) ? argv[2] : "asm/a.out";
+  success = write_binary_file(output, instructions, line_count);
+
+  if (success > 0)
+    printf("ASM Output Written to %s;  %d Instructions Written\n", output, success);
+  else
+    log_warn("NO INSTRUCTIONS WRITTEN TO ASM OUT");
 
   return 0;
+}
+
+WORD parse_hack_asm(char* line) {
+
+  WORD instruction = 0;
+
+  if (line[0] == '@') {
+    /* A-INSTRUCTION */
+    long long unsigned int num = 0;
+
+    if (sscanf(line, "@%lld", &num) != 1) {
+      log_warn(line);
+      fatal_error("INVALID HASM INSTRUCTION");
+    }
+    if (num > MAXADDR) {
+      log_warn(line);
+      fatal_error("INVALID MEMORY ADDRESS");
+    }
+    instruction = (WORD)num;
+  } else {
+    /* TODO: C-INSTRUCTION */
+
+  }
+
+  return instruction;
 }
--- source/assembler.h
+++ source/assembler.h
@@ -1,5 +1,11 @@
+#include "specification.h"
+
+
 /* Hack ASM Signals */
 #define ASM_SIG_QUIT  "QUIT"
 #define ASM_SIG_PRNTA "PRINT_A"
 #define ASM_SIG_PRNTD "PRINT_D"
 #define ASM_SIG_PRNTM "PRINT_M"
+
+
+WORD parse_hack_asm(char* line);
